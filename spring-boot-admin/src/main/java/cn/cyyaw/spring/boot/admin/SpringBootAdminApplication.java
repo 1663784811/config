8f92b401edaace15d390f51896a6fa4bae23dbc3
why <1663784811@qq.com>
2020-04-25 20:13:16 +0800

    init

--- spring-boot-admin/src/main/java/cn/cyyaw/spring/boot/admin/SpringBootAdminApplication.java
+++ spring-boot-admin/src/main/java/cn/cyyaw/spring/boot/admin/SpringBootAdminApplication.java
@@ -2,11 +2,13 @@ package cn.cyyaw.spring.boot.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-@SpringBootApplication
 @EnableAdminServer
+@EnableAutoConfiguration
+@EnableEurekaClient
 public class SpringBootAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAdminApplication.class, args);
