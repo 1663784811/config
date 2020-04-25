deda8d40ce9910be7448b3d245383c5c63cc5b0b
why <1663784811@qq.com>
2020-04-26 00:43:30 +0800

    init

---
+++ web-admin/src/main/java/cn/cyyaw/admin/table/WebAdminApplication.java
@@ -0,0 +1,17 @@
+package cn.cyyaw.admin.table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@SpringBootApplication
+public class WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminApplication.class, args);
+    }
+
+
+}
