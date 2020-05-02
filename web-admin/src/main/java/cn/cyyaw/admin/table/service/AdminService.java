e2a7fa2b96381b4655cbce2ad496dd9947c6163c
why <1663784811@qq.com>
2020-05-03 00:29:54 +0800

    init

--- web-admin/src/main/java/cn/cyyaw/admin/table/service/AdminService.java
+++ web-admin/src/main/java/cn/cyyaw/admin/table/service/AdminService.java
@@ -4,13 +4,7 @@ package cn.cyyaw.admin.table.service;
 import cn.cyyaw.admin.table.service.fallback.AdminServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "itoken-service-admin", fallback = AdminServiceFallback.class)
+@FeignClient(value = "cyyaw-service-admin", fallback = AdminServiceFallback.class)
 public interface AdminService {
-
-
-
-
-
-
 
 }
