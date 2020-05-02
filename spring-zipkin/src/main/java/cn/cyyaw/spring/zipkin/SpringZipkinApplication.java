9bfc3b013d332958c77f1bfa11610de454332969
why <1663784811@qq.com>
2020-05-03 01:58:06 +0800

    init

--- spring-zipkin/src/main/java/cn/cyyaw/spring/zipkin/SpringZipkinApplication.java
+++ spring-zipkin/src/main/java/cn/cyyaw/spring/zipkin/SpringZipkinApplication.java
@@ -3,12 +3,10 @@ package cn.cyyaw.spring.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.internal.EnableZipkinServer;
 
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZipkinServer
 public class SpringZipkinApplication {
 
     public static void main(String[] args) {
